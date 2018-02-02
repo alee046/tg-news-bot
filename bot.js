a5da4d69a879c2f51ba9aa8342de29632737461c
anthony lee <alee9011@gmail.com>
2018-02-01 16:16:47 -0800

    cleanup

--- bot.js
+++ bot.js
@@ -7,41 +7,31 @@ const newsUrl = 'https://newsapi.org/v2/top-headlines?sources=crypto-coins-news&
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(botToken, {polling: true});
 
-// Matches "/echo [whatever]"
 bot.onText(/\/echo (.+)/, (msg, match) => {
-  // 'msg' is the received Message from Telegram
-  // 'match' is the result of executing the regexp above on the text content
-  // of the message
 
   const chatId = msg.chat.id;
-  const resp = match[1]; // the captured "whatever"
-console.log(msg,match)
-  // send back the matched "whatever" to the chat
+  const resp = match[ 1 ];
   bot.sendMessage(chatId, resp);
 });
 
 bot.onText(/\/news/, (msg, match) => {
     got('https://newsapi.org/v2/top-headlines?sources=crypto-coins-news&apiKey=bb18fc4ffcf046589b0b8a743d72a314').then(function(result, error) {
-    match = JSON.parse(result.body); // "Stuff worked!"
-    const chatId = msg.chat.id;
-    console.log(match.articles[0])
-    for (i in match.articles){
-        bot.sendMessage(chatId, 
-`${match.articles[i].title}
-${match.articles[i].description} 
-${match.articles[i].url}
-@DeezyDoge 
-`);
-    }
-        // send a message to the chat acknowledging receipt of their message
-
+        match = JSON.parse( result.body ); 
+        const chatId = msg.chat.id;
+        for ( i in match.articles ) {
+            bot.sendMessage(chatId, 
+                `${match.articles[i].title}
+                ${match.articles[i].description} 
+                ${match.articles[i].url}
+                `
+            );
+        }
     }, function(err) {
-        console.log(err); // Error: "It broke"
+        console.log(err);
     });
 });
   
-// Listen for any kind of message. There are different kinds of
-// messages.
+
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
 
