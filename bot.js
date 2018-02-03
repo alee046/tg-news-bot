c4f7026921c7ec7f627cec34fa76cb8bb654c7ba
Anthony Lee <alee9011@gmail.com>
2018-02-02 16:19:04 -0800

    cleanup

--- bot.js
+++ bot.js
@@ -4,93 +4,69 @@ const FeedMe = require( 'feedme' );
 const parser = require( 'parse-rss' );
 const _ = require( 'lodash' );
 // const tokens = require('config.js)'
-const tokens = require('./config.js');
+const tokens = require( './config.js' );
 
 const newsUrl = 'https://newsapi.org/v2/top-headlines?sources=crypto-coins-news&apiKey=';
-console.log(tokens)
+const rssUrl = 'http://feed.informer.com/digests/I2GGLAVR70/feeder.rss';
+
 const bot = new TelegramBot( tokens.botToken, { polling: true } );
 
-bot.onText( /\/echo (.+)/, ( msg, match ) => {
-    const chatId = msg.chat.id;
-    const resp = match[ 1 ];
-    bot.sendMessage( chatId, resp );
-});
-
 bot.onText( /\/news/, ( msg, match ) => {
-    const matc = match[ 1 ];
     const chatId = msg.chat.id;
-
     bot.sendMessage( chatId, '-----Todays news----:' );
-    got( 'https://newsapi.org/v2/top-headlines?sources=crypto-coins-news&apiKey' + tokens.newsToken ).then( (result, error) => {
-        var match = JSON.parse( result.body ); 
-
-        for ( var i in match.articles ) {
+    got( newsUrl + tokens.newsToken ).then( (result, error) => {
+        let match = JSON.parse( result.body ); 
+        for ( let i in match.articles ) {
             bot.sendMessage( chatId, 
                 `${match.articles[i].title} : ${match.articles[i].url}`, 
                 { disable_web_page_preview : true }
             );
-        }
-
+        };
     }, ( err ) => {
         console.log( err );
     });
 });
 
-bot.onText( /\/rss/, ( msg, match ) => {
+bot.onText( /\/rss ([0-9]+)/, ( msg, match ) => {
     const chatId = msg.chat.id;
-    parser( "http://feed.informer.com/digests/I2GGLAVR70/feeder.rss", ( err, rss ) => {
-    let match =  rss;  
-    // match.forEach((r)=>{
-    //     console.log(r)
-    //     bot.sendMessage( chatId, 
-    //         `${r.title} : ${r.link}`, 
-    //         { disable_web_page_preview : true }
-    //     );
-    // });
-        for ( var i = 0; i < 5; i++ ) {
-            // Runs 5 times, with values of step 0 through 4.
-        //     console.log(r)
+    parser( rssUrl, ( err, rss ) => {
+    let res = rss,
+        num = match[1];
+        for ( let i = 0; i < num; i++ ) {
             bot.sendMessage( chatId, 
-                `${match[i].title} : ${match[i].link}`, 
+                `${res[i].title} : ${res[i].link}`, 
                 { disable_web_page_preview : true }
             );
-        }
-    }) 
-})
-bot.onText( /\/spamdeezy/, ( msg,match ) => {
+        };
+    }, ( err ) => {
+        console.log( err );
+    }); 
+});
+
+bot.onText( /\/spamdeezy ([0-9]+)/, ( msg, match ) => {
     const chatId = msg.chat.id;
-    parser( "http://feed.informer.com/digests/I2GGLAVR70/feeder.rss", ( err,rss ) => {
-    let match =  rss;  
-    // match.forEach((r)=>{
-    //     console.log(r)
-    //     bot.sendMessage( chatId, 
-    //         `${r.title} : ${r.link}`, 
-    //         { disable_web_page_preview : true }
-    //     );
-    // });
-        for ( var i = 0; i < 50; i++ ) {
-            bot.sendMessage( chatId,
-                `
-░░░░░░░█▐▓▓░████▄▄▄█▀▄▓▓▓▌█
-░░░░░▄█▌▀▄▓▓▄▄▄▄▀▀▀▄▓▓▓▓▓▌█
-░░░▄█▀▀▄▓█▓▓▓▓▓▓▓▓▓▓▓▓▀░▓▌█
-░░█▀▄▓▓▓███▓▓▓███▓▓▓▄░░▄▓▐█▌ 
-░█▌▓▓▓▀▀▓▓▓▓███▓▓▓▓▓▓▓▄▀▓▓▐█
-▐█▐██▐░▄▓▓▓▓▓▀▄░▀▓▓▓▓▓▓▓▓▓▌█▌
-█▌███▓▓▓▓▓▓▓▓▐░░▄▓▓███▓▓▓▄▀▐█ 
-█▐█▓▀░░▀▓▓▓▓▓▓▓▓▓██████▓▓▓▓▐█ 
-▌▓▄▌▀░▀░▐▀█▄▓▓██████████▓▓▓▌█▌
-▌▓▓▓▄▄▀▀▓▓▓▀▓▓▓▓▓▓▓▓█▓█▓█▓▓▌█▌
-█▐▓▓▓▓▓▓▄▄▄▓▓▓▓▓▓█▓█▓█▓█▓▓▓▐
-                `, 
-                { disable_web_page_preview : true });
-        }
-    })
-})
+    const num = match[1]; 
+
+    for ( let i = 0; i < num; i++ ) {
+        bot.sendMessage( chatId,
+            `
+        ░░░░░░░█▐▓▓░████▄▄▄█▀▄▓▓▓▌█
+        ░░░░░▄█▌▀▄▓▓▄▄▄▄▀▀▀▄▓▓▓▓▓▌█
+        ░░░▄█▀▀▄▓█▓▓▓▓▓▓▓▓▓▓▓▓▀░▓▌█
+        ░░█▀▄▓▓▓███▓▓▓███▓▓▓▄░░▄▓▐█▌ 
+        ░█▌▓▓▓▀▀▓▓▓▓███▓▓▓▓▓▓▓▄▀▓▓▐█
+        ▐█▐██▐░▄▓▓▓▓▓▀▄░▀▓▓▓▓▓▓▓▓▓▌█▌
+        █▌███▓▓▓▓▓▓▓▓▐░░▄▓▓███▓▓▓▄▀▐█ 
+        █▐█▓▀░░▀▓▓▓▓▓▓▓▓▓██████▓▓▓▓▐█ 
+        ▌▓▄▌▀░▀░▐▀█▄▓▓██████████▓▓▓▌█▌
+        ▌▓▓▓▄▄▀▀▓▓▓▀▓▓▓▓▓▓▓▓█▓█▓█▓▓▌█▌
+        █▐▓▓▓▓▓▓▄▄▄▓▓▓▓▓▓█▓█▓█▓█▓▓▓▐
+            `, 
+        { disable_web_page_preview : true });
+    }
+});
 
 bot.on( 'message', ( msg ) => {
   const chatId = msg.chat.id;
-
   // send a message to the chat acknowledging receipt of their message
-  
 });
